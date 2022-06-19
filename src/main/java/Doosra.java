5d10af520662fc6b49c6b5bfd1765550695f06ba
murgeshnb <mnbechetti@gmail.com>
2022-06-20 00:35:55 +0530

    added maga and doosra files.. for comparison purpose..

--- src/main/java/Doosra.java
+++ src/main/java/Doosra.java
@@ -1,2 +1,5 @@
 public class Doosra {
+    public static void main(String[] args) {
+        System.out.println("magaaa... main");
+    }
 }
--- src/test/java/Magaa.java
+++ src/test/java/Magaa.java
@@ -1,2 +1,6 @@
 public class Magaa {
+
+    public static void main(String[] args) {
+        System.out.println("Hii le shishya...");
+    }
 }
